59451fc70d86fa5f30614b1146fa290bd090b15a
iampolicy <iampolicy@users.noreply.github.com>
2026-09-14 14:42:28 +0000

    Support Aftman in Windows setup

--- setup.ps1
+++ setup.ps1
@@ -1,13 +1,27 @@
 $ErrorActionPreference = "Stop"
 Set-Location $PSScriptRoot
 
-Write-Host "[1/3] Trusting Wally in Rokit"
-rokit trust UpliftGames/wally
+$rokit = Get-Command rokit -ErrorAction SilentlyContinue
+$aftman = Get-Command aftman -ErrorAction SilentlyContinue
 
-Write-Host "[2/3] Installing pinned tools"
-rokit install
+if ($rokit) {
+    Write-Host "[1/3] Using Rokit"
+    rokit trust UpliftGames/wally
+    rokit install
+} elseif ($aftman) {
+    Write-Host "[1/3] Using Aftman"
+    aftman install
+} else {
+    throw "Neither Rokit nor Aftman is available in PATH. Install one of them before continuing."
+}
 
-Write-Host "[3/3] Installing Wally packages"
+Write-Host "[2/3] Verifying Wally"
+$wally = Get-Command wally -ErrorAction SilentlyContinue
+if (-not $wally) {
+    throw "Wally was not installed or is not available in PATH after toolchain setup."
+}
+
+Write-Host "[3/3] Installing React Lua packages"
 wally install
 
 Write-Host "EverLeaf Roblox development dependencies are ready."
